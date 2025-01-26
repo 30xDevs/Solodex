5a6857717097aba7a51da15284aab472e403f13b
imeyer2 <130110431+imeyer2@users.noreply.github.com>
2025-01-26 15:45:56 -0600

    Update urls.py

--- solodex/myapp/urls.py
+++ solodex/myapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_person/', views.add_person, name='add_person'), 
     # endpoint for adding a person
-    path('submit_form/', views.submit_form, name='submit_form'), 
+    path('add_person/', views.add_person, name='add_person'), 
     # endpoint for the add_person.html submission. New person to DB
+    path('submit_form/', views.submit_form, name='submit_form'), 
+    # endpoint for fetching the csrf token, since it may not be stored in cookies using electron
     path('get-csrf-token/', views.get_csrf_token, name='get-csrf-token'),
-
 ]
