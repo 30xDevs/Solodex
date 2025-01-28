dcdabff2a8ea03e9eb00e28a985d7fcc4c3252f5
Jackson Myers <jacksonthomyers@gmail.com>
2025-01-27 21:24:05 -0600

    CORS headers working

--- solodex/solodex-conf/settings.py
+++ solodex/solodex-conf/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:1420",
+    "http://127.0.0.1:1420"
 ]
 
 ROOT_URLCONF = 'solodex-conf.urls'
