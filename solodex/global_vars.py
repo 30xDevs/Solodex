f1ffec645761c4784f3eba82bd728cc9b4436c79
Ian Meyer <imeyer@unomaha.edu>
2025-02-10 21:15:03 -0600

    need global vars for ollama functionality in the backend'

--- solodex/global_vars.py
+++ solodex/global_vars.py
@@ -1,2 +1,7 @@
+import subprocess
 
-ollama_process_object = None
+ollama_process_object : subprocess.Popen  = None
+
+def update_global_variable(new_value):
+    global ollama_process_object
+    ollama_process_object = new_value
